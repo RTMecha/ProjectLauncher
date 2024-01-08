a87c994fede40c710e36c492da47996b9c3988cb
RTMecha <mechaprower2022@gmail.com>
2024-01-08 22:23:41 +1100

    TryFind

---
+++ Functions/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLauncher.Functions
+{
+    public static class Extensions
+    {
+        public static bool TryFind<T>(this List<T> values, Predicate<T> predicate, out T result)
+        {
+            var e = values.Find(predicate);
+            if (e != null)
+            {
+                result = e;
+                return true;
+            }
+
+            result = default;
+            return false;
+        }
+    }
+}
