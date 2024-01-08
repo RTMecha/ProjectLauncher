994adfc58154a29c3de185b186ed87528385bf26
RTMecha <mechaprower2022@gmail.com>
2024-01-08 16:32:36 +1100

    Early beginnings on custom themes.

---
+++ Functions/Themes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLauncher.Functions
+{
+    public static class Themes
+    {
+        public static Theme DefaultTheme { get; set; } = new Theme("Default", "The default theme.", RTColor.FromHex("211F1D"));
+
+        public static Theme CurrentTheme { get; set; } = DefaultTheme;
+
+        public static Dictionary<string, Theme> ThemesDictionary { get; set; } = new Dictionary<string, Theme>
+        {
+            { "Default", DefaultTheme }
+        };
+
+        public class Theme
+        {
+            public Theme(string name, string description, RTColor backgroundColor)
+            {
+                Name = name;
+                Description = description;
+                BackgroundColor = backgroundColor;
+            }
+
+            public string Name { get; set; }
+            public string Description { get; set; }
+
+            public RTColor BackgroundColor { get; set; }
+        }
+    }
+}
